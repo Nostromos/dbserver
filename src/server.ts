eafb1629f40a37e96bf3546c43e7fc7c37d5018f
Michael Monaghan <Nostromos@users.noreply.github.com>
2025-04-02 11:13:48 -0400

    Make sure we can start and stop the server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import * as http from "http";
+import type { Server } from "http";
 
 const PORT = 4000;
 
@@ -7,3 +8,27 @@ const server = http.createServer();
 
 // Listen to requests
 server.listen(PORT);
+
+class babyServer {
+  port: number;
+  memory: Map<string, string>;
+  server: Server | null;
+
+  constructor(port = 4000) {
+    this.port = port;
+    this.memory = new Map();
+    this.server = null;
+  }
+
+  start = (): void => {
+    this.server = http.createServer();
+    this.server.listen(this.port);
+    console.log(`[babyServer] Server started and listening on port ${this.port}.`)
+  }
+
+  stop = (): void => {
+    this.server?.close();
+    this.server?.closeAllConnections();
+    console.log(`[babyServer] Server on port ${this.port} closed successfully.`)
+  }
+}
