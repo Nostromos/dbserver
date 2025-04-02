2e8a382f7a793243102382075656f985820cfad6
Michael Monaghan <Nostromos@users.noreply.github.com>
2025-04-02 15:06:48 -0400

    Add some tests

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import babyServer from "./server.js";
 
-const PORT = 4000;
-
-const bbs = new babyServer(PORT);
+const bbs = new babyServer();
 
 bbs.start();
 
-const request = "get this";
+const first = await fetch("http://localhost:4000/set?test1=test1");
+const second = await fetch("http://localhost:4000/get?key=test1");
 
-const response = await fetch("http://localhost:4000/get?key=somekey");
+const third = await fetch("http://localhost:4000/set?3=three");
+const fourth = await fetch("http://localhost:4000/set?4=four");
+const fifth = await fetch("http://localhost:4000/set?5=five");
+
+const sixth = await fetch("http://localhost:4000/get?key=3")
+  .then(async (res) => {
+    let str = await res.text();
+    console.log(str);
+});
+const seventh = await fetch("http://localhost:4000/get?key=4");
+const eighth = await fetch("http://localhost:4000/get?key=5");
+
